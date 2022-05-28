94abcb2de3a3985c7972aa7611aa9096ee7ea534
Saurabh Kumar Singh <saurabhkumaarsingh01@gmail.com>
2022-05-28 08:08:19 +0530

    added javascript file

---
+++ index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View>
+      <Text>Second file in this project .</Text>
+    </View>
+  )
+}
